7a68ac82b30ff4982264c8b9a10b9b05b0a4a04f
Carlos-Danilo <carlos.danilo@escolar.ifrn.edu.br>
2025-10-02 14:42:19 +0000

    mais dor e sofrimento

--- views.py
+++ views.py
@@ -4,15 +4,30 @@ from models.Horario import Horario, HorarioDAO
 from models.profissional import Profissional, ProfissionalDAO
 
 class View:
+
+    
+    def cliente_criar_admin():
+        for c in View.cliente_listar():
+            if c.get_email() == "admin": return
+        View.cliente_inserir("admin", "admin", "fone", "1234")
+
+    def cliente_autenticar(email, senha):
+        for c in View.cliente_listar():
+            if c.get_email() == email and c.get_senha() == senha:
+                return {"id": c.get_id(), "nome": c.get_nome()}
+        return None
+
+
+
     def cliente_listar():
         return ClienteDAO.listar()
-    def cliente_inserir(nome, email, fone):
-        cliente = Cliente(0, nome, email, fone)
+    def cliente_inserir(nome, email, fone, senha):
+        cliente = Cliente(0, nome, email, fone, senha)
         ClienteDAO.inserir(cliente)
     def cliente_listar_id(id):
         return ClienteDAO.listar_id(id)
-    def cliente_atualizar(id, nome, email, fone):
-        cliente = Cliente(id, nome, email, fone)
+    def cliente_atualizar(id, nome, email, fone, senha):
+        cliente = Cliente(id, nome, email, fone, senha)
         ClienteDAO.atualizar(cliente)
     def cliente_excluir(id):
         cliente = Cliente(id, "", "", "")
